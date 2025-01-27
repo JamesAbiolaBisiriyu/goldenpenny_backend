3358224a006d51f185e156b70bde007b0f5983d5
james <emperor2g@gmail.com>
2025-01-27 14:55:10 +0100

    3:55

---
+++ routes/foodRoute.js
@@ -0,0 +1,15 @@
+import express from "express"
+import { addFood } from "../controllers/foodController.js"
+import multer from "multer"  //used for creating image storage system
+
+
+const foodRouter = express.Router();
+
+
+food
+
+
+
+
+
+export default foodRouter;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import { connectdb } from "./config/db.js"
+import foodRouter from "./routes/foodRoute.js"
 
 
 
@@ -15,6 +16,9 @@ app.use(express.json())
 app.use(cors())
 //db connection
 connectdb();
+
+//api endpoints
+app.use("/api/food",foodRouter)
 
 app.get("/",(req, res)=>{
   res.send("API Working")
